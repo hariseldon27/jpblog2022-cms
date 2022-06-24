6c823e392b4af38c22b9fece322e69a6d6adcfc5
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-06-23 17:05:38 -0700

    middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -37,11 +37,11 @@ module.exports = [
   {
     name: 'strapi::body',
     config: {
-      formLimit: "256mb", // modify form body
-      jsonLimit: "256mb", // modify JSON body
-      textLimit: "256mb", // modify text body
+      formLimit: "256mb", 
+      jsonLimit: "256mb", 
+      textLimit: "256mb", 
       formidable: {
-        maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+        maxFileSize: 200 * 1024 * 1024, 
       }
     }
   },
