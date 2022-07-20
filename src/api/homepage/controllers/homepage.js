7be10fe65ae6e80bce6988c63551f73d8b9730c4
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-07-20 11:40:30 -0700

    blogpost schema circles

--- src/api/homepage/controllers/homepage.js
+++ src/api/homepage/controllers/homepage.js
@@ -40,6 +40,7 @@ const getFullPopulateObject = (modelUid, maxDepth = 20) => {
       if (value.type === "component") {
         console.log('+++++COMPONENT++++')
         populate[key] = getFullPopulateObject(value.component, maxDepth - 1);
+        console.log(`!!!POPULATE!!!!`, populate)
       } else if (value.type === "dynamiczone") {
         console.log('*****DYNAMIC*****')
         const dynamicPopulate = value.components.reduce((prev, cur) => {
@@ -47,6 +48,8 @@ const getFullPopulateObject = (modelUid, maxDepth = 20) => {
           return curPopulate === true ? prev : merge(prev, curPopulate);
         }, {});
         populate[key] = isEmpty(dynamicPopulate) ? true : dynamicPopulate;
+        console.log(`!!!POPULATE!!!!`, populate)
+        
       } else if (value.type === "relation") {
         console.log('<<<<<RELATION>>>>>')
         
@@ -57,17 +60,22 @@ const getFullPopulateObject = (modelUid, maxDepth = 20) => {
         if (relationPopulate) {
           populate[key] = relationPopulate;
         }
+        console.log(`!!!POPULATE!!!!`, populate)
+
       } else if (value.type === "media") {
         console.log('>>>>>MEDIA<<<<<<')
         // console.log(value.type)
         populate[key] = true;
+        console.log(`!!!POPULATE!!!!`, populate)
+
       }
     }
   }
+  console.log(`populate obj: `, populate)
   return isEmpty(populate) ? true : { populate };
 };
 
-const modelUid = "api::homepage.homepage";
+const modelUid = "api::blogpost.blogpost";
 
 module.exports = createCoreController(modelUid, ({ strapi }) =>  ({
 
@@ -96,7 +104,13 @@ module.exports = createCoreController(modelUid, ({ strapi }) =>  ({
               ...getFullPopulateObject(modelUid),
               ...query,
             });
-        
+            console.log('*********************')
+
+            console.log('query: ', query)
+            console.log(`meta: `, meta)
+            console.log(`results: `, results)
+            console.log('*********************')
+
             const sanitizedEntities = await this.sanitizeOutput(results, ctx);
         
             return {
