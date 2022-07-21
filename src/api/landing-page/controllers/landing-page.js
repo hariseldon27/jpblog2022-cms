9fe67a98de420f7c7a9586f2e7eb3d077888f614
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-07-21 14:05:59 -0700

    populate and route worky

--- src/api/landing-page/controllers/landing-page.js
+++ src/api/landing-page/controllers/landing-page.js
@@ -1,9 +1,73 @@
-'use strict';
+// 'use strict';
+
+// /**
+//  *  landing-page controller
+//  */
+
+// const { createCoreController } = require('@strapi/strapi').factories;
+
+// module.exports = createCoreController('api::landing-page.landing-page');
+
+"use strict";
 
 /**
- *  landing-page controller
+ *  custom landing-page controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::landing-page.landing-page');
+module.exports = createCoreController("api::landing-page.landing-page", ({ strapi }) => ({
+
+    // async findOne(ctx) {
+    //     const entry = await strapi.entityService.findOne('api::landing-page.landing-page', 1, {
+    //         populate: { someRelation: true },
+    //         });
+    // },
+    async findOne(ctx) {
+        const { page_name } = ctx.params
+        // console.log(ctx)
+        // console.log(ctx.params.id)
+        const query = {
+            filters: { page_name },
+            ...ctx.query,
+        };
+        // console.log(query)
+        const pageFind = await strapi.entityService.findMany("api::landing-page.landing-page", query);
+        
+ 
+        console.log(pageFind[0].id)
+
+        const returnQuery = {
+            populate: {
+                page_body: {
+                    populate: {
+                        source: true,
+                        image: true,
+                    }
+                },
+                blogposts: {
+                    populate: {
+                        title: true,
+                    },
+                }
+            },
+            ...ctx.query
+        }
+
+        const returnPage = await strapi.entityService.findOne("api::landing-page.landing-page", pageFind[0].id, returnQuery)
+        console.log('VVVVVVVVVV')
+        console.log('VV RAW  VV')
+        console.log(returnPage)
+        console.log('^^^^^^^^^^^^^^^^')
+
+        const sanitizedEntity = await this.sanitizeOutput(returnPage);
+
+        console.log('VVVVVVVVVVVVVVVV')
+        console.log('VV SANITIZED  VV')
+        console.log(sanitizedEntity)
+        console.log('^^^^^^^^^^^^^^^^')
+
+
+        return this.transformResponse(returnPage);
+    },
+}));
---
+++ src/api/landing-page/routes/custom.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/**
+ * custom landing-page router.
+ */
+
+const { createCoreRouter } = require("@strapi/strapi").factories;
+const defaultRouter = createCoreRouter("api::landing-page.landing-page");
+
+// function to add to or override default router methods
+
+const customRouter = (innerRouter, routeOveride = [], extraRoutes = []) => {
+  let routes;
+
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes;
+
+      const newRoutes = routes.map((route) => {
+        let found = false;
+
+        routeOveride.forEach((overide) => {
+          if (
+            route.handler === overide.handler &&
+            route.method === overide.method
+          ) {
+            found = overide;
+          }
+        });
+
+        return found || route;
+      });
+
+      return newRoutes.concat(extraRoutes);
+    },
+  };
+};
+
+// Overide the default router with the custom router to use slug.
+const myOverideRoutes = [
+  {
+    method: "GET",
+    path: "/landing-pages/:page_name",
+    handler: "api::landing-page.landing-page.findOne",
+  },
+];
+
+const myExtraRoutes = [];
+
+module.exports = customRouter(defaultRouter, myOverideRoutes, myExtraRoutes);
