fbcf79d36ab5567af692a20668de53f387508b91
Jonathan Potter <jonathan@mrjonathanpotter.com>
2022-07-24 14:27:25 -0700

    cleanup

--- src/api/landing-page/controllers/landing-page.js
+++ src/api/landing-page/controllers/landing-page.js
@@ -20,13 +20,12 @@ module.exports = createCoreController("api::landing-page.landing-page", ({ strap
 
     async findOne(ctx) {
         const { page_name } = ctx.params
-        // console.log(ctx)
-        // console.log(ctx.params.id)
+
         const query = {
             filters: { page_name },
             ...ctx.query,
         };
-        // console.log(query)
+
         const pageFind = await strapi.entityService.findMany("api::landing-page.landing-page", query);
         
  
@@ -51,18 +50,8 @@ module.exports = createCoreController("api::landing-page.landing-page", ({ strap
         }
 
         const returnPage = await strapi.entityService.findOne("api::landing-page.landing-page", pageFind[0].id, returnQuery)
-        // console.log('VVVVVVVVVV')
-        // console.log('VV RAW  VV')
-        // console.log(returnPage)
-        // console.log('^^^^^^^^^^^^^^^^')
 
         const sanitizedReturn = await this.sanitizeOutput(returnPage);
-
-        // console.log('VVVVVVVVVVVVVVVV')
-        // console.log('VV SANITIZED  VV')
-        // console.log(sanitizedEntity)
-        // console.log('^^^^^^^^^^^^^^^^')
-
 
         return this.transformResponse(returnPage);
     },
